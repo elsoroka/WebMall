2148786eae51d697a6f5f0f99d31113203fdaef1
Emi Soroka <emiko@me.com>
2026-03-29 15:38:51 -0700

    Generate a jsonl file of task prompts (hack) to test planning. Then generate plans only (another hack).

---
+++ plan_only/test_plan_only.py
@@ -0,0 +1,31 @@
+import json
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+from tqdm import tqdm
+
+client = openai.OpenAI()
+
+def chat(prompt: str, model="gpt-5.1", temp=0.0) -> str:
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=temp
+    )
+    return response.choices[0].message.content.strip()
+
+
+if __name__ == '__main__':
+    with open("webmall_prompts.jsonl", "r") as infile:
+        prompts = [json.loads(l) for l in infile.readlines()]
+
+    MODEL = "gpt-4.1"
+    TEMP = 0.0
+    with open(f"webmall_plan_{MODEL}_{TEMP}.jsonl", "w") as outfile:
+
+        for p in tqdm(prompts):
+            response = chat(p['prompt'])
+            p['response'] = response
+            outfile.write(json.dumps(p) + '\n')
