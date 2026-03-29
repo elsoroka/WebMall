a49de56a4e8e2bab8746fb44d1436f18491c4b23
Emi Soroka <emiko@me.com>
2026-03-29 16:46:36 -0700

    add setting model in plan only

--- plan_only/test_plan_only.py
+++ plan_only/test_plan_only.py
@@ -21,11 +21,11 @@ if __name__ == '__main__':
     with open("webmall_prompts.jsonl", "r") as infile:
         prompts = [json.loads(l) for l in infile.readlines()]
 
-    MODEL = "gpt-4.1"
+    MODEL = "gpt-5.1"
     TEMP = 0.0
     with open(f"webmall_plan_{MODEL}_{TEMP}.jsonl", "w") as outfile:
 
         for p in tqdm(prompts):
-            response = chat(p['prompt'])
+            response = chat(p['prompt'], model=MODEL, temp=TEMP)
             p['response'] = response
             outfile.write(json.dumps(p) + '\n')
