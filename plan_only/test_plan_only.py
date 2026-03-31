39f669805086a690a1737eff088e550571b360e5
Emi Soroka <emiko@me.com>
2026-03-31 13:51:55 -0700

    update test_plan_only to match colab functions

--- plan_only/test_plan_only.py
+++ plan_only/test_plan_only.py
@@ -4,6 +4,43 @@ from dotenv import load_dotenv
 load_dotenv()
 from tqdm import tqdm
 
+example = """
+Example: Make a plan to find the cheapest offer for Product P.
+```
+stores = ["http://localhost:8081/", "http://localhost:8082/", "http://localhost:8083/", "http://localhost:8084/"]
+results = []
+
+for store in stores:
+    url_or_none = search_for_page(store, "Product P") # Return the product page URL or None if not found
+    if url_or_none is not None:
+        price = extract_information_from_page("Lowest price of the product")
+        results.append((url_or_none, price))
+
+selected_url = min(results, key=lambda x: x[1])[0]
+
+open_page("http://localhost:3000/")
+fill_text_field("Solution field", selected_url)
+press_button("Submit Final Result")
+```"""
+
+def get_first_valid(response:str)->str:
+  response = response.strip()
+  if '```' in response:
+    s = response.split('```')
+    for i in range(len(s)):
+      if len(s[i].strip()) == 0:
+        continue
+      if s[i].startswith("python"):
+        s[i] = s[i][6:].strip() # clip this off
+      try:
+        byte_code = compile(s[i], "<user_code>", "exec")
+        print(s[i])
+        return s[i]
+      except Exception as e:
+        print(e)
+  return None
+
+
 client = openai.OpenAI()
 
 def chat(prompt: str, model="gpt-5.1", temp=0.0) -> str:
@@ -23,9 +60,11 @@ if __name__ == '__main__':
 
     MODEL = "gpt-5.1"
     TEMP = 0.0
-    with open(f"webmall_plan_{MODEL}_{TEMP}.jsonl", "w") as outfile:
+    EXAMPLE = True
+    with open(f"webmall_plan_{MODEL}_{TEMP}_Example_{EXAMPLE}.jsonl", "w") as outfile:
 
         for p in tqdm(prompts):
-            response = chat(p['prompt'], model=MODEL, temp=TEMP)
+            response = chat(p['prompt'] + (example + '\n' if EXAMPLE else ''), model=MODEL, temp=TEMP)
             p['response'] = response
+            p['clean_response'] = get_first_valid(response)
             outfile.write(json.dumps(p) + '\n')
